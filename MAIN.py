99c7e0dfcc44d542ad169c7dee2ac19ebc3fb397
uvwbermudo <100179424+uvwbermudo@users.noreply.github.com>
2022-06-01 22:12:26 +0800

    Autofills

--- MAIN.py
+++ MAIN.py
@@ -1,11 +1,12 @@
 from ast import While
+from tracemalloc import start
 import mysql 
 import mysql.connector 
 import sys 
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QTableWidgetItem, QHeaderView, QErrorMessage, QPushButton, QHBoxLayout, QMessageBox
-from PyQt5 import uic, QtCore   
+from PyQt5 import uic, QtCore
 import os 
-
+import datetime
 #cursor for manipulating database
 db = mysql.connector.connect(host = 'localhost', user = 'root', password = '*P@ssw0rd', database = 'book_rental') 
 mydb = db.cursor() 
@@ -13,17 +14,124 @@ mydb = db.cursor()
 # to fix gui not displaying properly on different resolutions
 QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True) 
 
+def convert_to_sql(qtimedate, alt = False):        #convert Time date from QT to SQL Format if alt is true, convert to python DateTime format instead
+    qtimedate = qtimedate.split(' ')
+    year = int(qtimedate[4])
+    time = qtimedate[3]
+    day = int(qtimedate[2])
+    month =''
+    if qtimedate[1] == 'Jan':
+        month = 1
+    elif qtimedate[1] == 'Feb':
+        month = 2
+    elif qtimedate[1] == 'Mar':
+        month = 3
+    elif qtimedate[1] == 'Apr':
+        month = 4
+    elif qtimedate[1] == 'May':
+        month = 5 
+    elif qtimedate[1] == 'Jun': 
+        month = 6 
+    elif qtimedate[1] == 'Jul': 
+        month = 7 
+    elif qtimedate[1] == 'Aug': 
+        month = 8 
+    elif qtimedate[1] == 'Sep': 
+        month = 9 
+    elif qtimedate[1] == 'Oct': 
+        month = 10 
+    elif qtimedate[1] == 'Nov': 
+        month = 11 
+    elif qtimedate[1] == 'Dec': 
+        month = 12 
+
+    if alt == True:
+        time = time.split(':')
+        hour = int(time[0])
+        minutes = int(time[1])
+        secs = int(time[2])
+        secs = int(secs)
+        return [(year), month, day, hour, minutes, secs, 0]
+
+    return f"{year}-{month}-{day} {time}" 
+
+#Sat Jan 1 00:00:00 2000 Q FORMAT 
+#1999-07-22 12:00:00 SQL RETURN 
+#'1999-07-24 12:00:00' SQL INSERT 
 
 class RentBook(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi(f'{sys.path[0]}/rent_form.ui', self) 
+        self.cust_complete.pressed.connect(self.complete)
+        self.cust_id.editingFinished.connect(self.autofill_cust)
+        self.cust_due.editingFinished.connect(self.getprice)
+        self.cust_start.setMinimumDateTime(QtCore.QDateTime.currentDateTime())
+        self.cust_due.setMinimumDateTime(QtCore.QDateTime.currentDateTime().addDays(1))
+        self.cust_start.setDateTime(QtCore.QDateTime.currentDateTime())
+        self.cust_due.setDateTime(QtCore.QDateTime.currentDateTime().addDays(1))
+        self.cust_startdate = '' 
+        self.cust_duedate = '' 
+        self.book_title = '' 
+        self.book_price = '' 
+        self.book_cpyno = '' 
+        self.customer_id = ''
+        self.cust_alrexist = False
 
-    def autofill_book(self, book_price, book_cpyno, book_title):
-        self.cust_price.setText(str(book_price))
-        self.cust_bookrent_id.setText(str(book_cpyno))
-        self.cust_bookrent_title.setText(str(book_title))
+    def complete(self):
+        customer_name = self.cust_name.text()
+        phone_no = self.cust_phone.text()
+        street_add = self.cust_street.text()
+        brgy_add = self.cust_barangay.text()
+        house_add = self.cust_house.text()
+        self.customer_id = self.cust_id.text()
+        self.cust_startdate = self.cust_start.dateTime().toString()
+        self.cust_duedate = self.cust_due.dateTime().toString()
+        start_date = convert_to_sql(self.cust_startdate)
+        due_date = convert_to_sql(self.cust_duedate)
+        self.cust_startdate = convert_to_sql(self.cust_start.dateTime().toString())
+        self.cust_duedate = convert_to_sql(self.cust_due.dateTime().toString())
+        self.book_cpyno = self.cust_bookrent_id.text()
+        mydb.execute(f"INSERT INTO customer VALUES({self.customer_id},'{customer_name}', 'None', '{phone_no}', '{street_add}', '{brgy_add}', {house_add})")
+        db.commit()
+        mydb.execute(f"INSERT INTO rents VALUES({self.customer_id}, {self.book_cpyno}, {self.book_price},'{start_date}', NULL , '{due_date}', NULL)")
+        db.commit() 
+        self.close() 
     
+    def autofill_cust(self): 
+        self.customer_id = self.cust_id.text() 
+        mydb.execute(f"SELECT customer_id, full_name, id_image, phone_number, street, barangay, house_no FROM customer WHERE customer_id = '{self.customer_id}'")
+        rows = mydb.fetchone()
+        if rows == None:
+            return
+        self.cust_alrexist = True
+        self.cust_name.setText(rows[1])
+        self.cust_idpic.setText(rows[2])
+        self.cust_phone.setText(rows[3])
+        self.cust_street.setText(rows[4])
+        self.cust_barangay.setText(rows[5])
+        self.cust_house.setText(str(rows[6]))
+        
+
+    def autofill_book(self):
+        self.cust_price.setText(str(self.book_price))
+        self.cust_bookrent_title.setText(str(self.book_title))
+        print('TITLE TEST',self.book_title)
+    
+    def getprice(self):
+        self.cust_startdate = self.cust_start.dateTime().toString()
+        self.cust_duedate = self.cust_due.dateTime().toString()
+        start_time = convert_to_sql(self.cust_startdate, True)
+        start_time = datetime.datetime(start_time[0], start_time[1], start_time[2], start_time[3], start_time[4], start_time[5], start_time[6])
+        due_time = convert_to_sql(self.cust_duedate, True)
+        due_time = datetime.datetime(due_time[0], due_time[1], due_time[2], due_time[3], due_time[4], due_time[5], due_time[6])
+        print(start_time)
+        print(due_time)
+        rent_days = str((due_time - start_time))
+        rent_days = int(rent_days.split()[0])
+
+        rent_price = rent_days*self.book_price
+        self.cust_price.setText(str(rent_price))
 
 class EditBook(QMainWindow):
     def __init__(self):
@@ -103,7 +211,17 @@ class MainWindow (QMainWindow):
         book_price = rows[1]
         book_cpyno = rows[2]
         book_title = rows[0]
-        self.rent_window.autofill_book(book_price,book_cpyno,book_title)
+        self.rent_window.cust_id.clear()
+        self.rent_window.cust_name.clear()
+        self.rent_window.cust_phone.clear()
+        self.rent_window.cust_idpic.clear()
+        self.rent_window.cust_street.clear()
+        self.rent_window.cust_barangay.clear()
+        self.rent_window.cust_house.clear()
+        self.rent_window.book_price = book_price
+        self.rent_window.book_cpyno = book_cpyno
+        self.rent_window.book_title = book_title
+        self.rent_window.autofill_book()
         self.rent_window.show()
     
     def open_addbook(self):                                             #! PA IMPLEMENT KO SOPHIA
